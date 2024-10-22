35a225da4ac9b02d396978c65bf39e94ff339bd0
Murilo Calegari <murilo478@gmail.com>
2024-10-22 11:11:37 -0400

    Add base HelloWorld test with launch.json

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ brew update
 
 # Install CMake using Homebrew
 brew install cmake
+brew install gdb
 
 # Verify the installation
 if brew list cmake &>/dev/null; then
@@ -13,3 +14,10 @@ else
     echo "There was an error installing cmake."
 fi
 
+
+# Verify gdb
+if brew list gdb &>/dev/null; then
+    echo "gdb has been successfully installed."
+else
+    echo "There was an error installing gdb."
+fi
