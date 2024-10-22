9a2723770fd8b9c2c39f283eda07e932cbc3cfb3
Murilo Calegari <murilo478@gmail.com>
2024-10-22 11:12:14 -0400

    Remove gdb from setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ brew update
 
 # Install CMake using Homebrew
 brew install cmake
-brew install gdb
 
 # Verify the installation
 if brew list cmake &>/dev/null; then
@@ -14,10 +13,3 @@ else
     echo "There was an error installing cmake."
 fi
 
-
-# Verify gdb
-if brew list gdb &>/dev/null; then
-    echo "gdb has been successfully installed."
-else
-    echo "There was an error installing gdb."
-fi
