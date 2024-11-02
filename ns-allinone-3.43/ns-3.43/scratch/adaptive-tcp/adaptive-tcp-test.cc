7ba0d3e8c6813fc360b7952a279147dd52df15e3
Murilo Calegari <murilo478@gmail.com>
2024-11-02 15:38:37 -0400

    Add test

--- ns-allinone-3.43/ns-3.43/scratch/adaptive-tcp/adaptive-tcp-test.cc
+++ ns-allinone-3.43/ns-3.43/scratch/adaptive-tcp/adaptive-tcp-test.cc
@@ -17,21 +17,83 @@
 // defined in the "scratch-nested-subdir-library-header.h" header file and
 // implemented in "scratch-nested-subdir-library-source.cc".
 
-#include "lib/scratch-nested-subdir-library-header.h"
-
 #include "ns3/core-module.h"
-
-#include <string>
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "adaptive-tcp-test.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("ScratchNestedSubdir");
+NS_LOG_COMPONENT_DEFINE("AdaptiveTcpTest");
 
 int
 main(int argc, char* argv[])
 {
-    std::string message = ScratchNestedSubdirGetMessage();
-    NS_LOG_UNCOND(message);
+    std::string linkBandwidth = "1000Mbps"; // Default to 1Gbps
+    double simulationTime = 300.0; // Default to 5 minutes
+
+    CommandLine cmd;
+    cmd.AddValue("linkBandwidth", "Bandwidth of the middle link", linkBandwidth);
+    cmd.AddValue("simulationTime", "Simulation runtime in seconds", simulationTime);
+    cmd.Parse(argc, argv);
+
+    NodeContainer nodes;
+    nodes.Create(2);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue(linkBandwidth));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NetDeviceContainer devices;
+    devices = pointToPoint.Install(nodes);
+
+    InternetStackHelper stack;
+    stack.Install(nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer interfaces = address.Assign(devices);
+
+    for (int i = 0; i < CCA_COUNT; i++) {
+        int numSenders = ccaData[i].percentage;
+        for (int j = 0; j < numSenders; j++) {
+            // Create sender and receiver nodes
+            Ptr<Node> sender = CreateObject<Node>();
+            Ptr<Node> receiver = CreateObject<Node>();
+
+            // Install internet stack on sender and receiver
+            stack.Install(sender);
+            stack.Install(receiver);
+
+            // Create point-to-point link between sender and receiver
+            NetDeviceContainer senderReceiverDevices;
+            senderReceiverDevices = pointToPoint.Install(sender, receiver);
+
+            // Assign IP addresses
+            Ipv4InterfaceContainer senderReceiverInterfaces;
+            senderReceiverInterfaces = address.Assign(senderReceiverDevices);
+
+            // Set TCP type
+            TypeId tcpTypeId = TypeId::LookupByName(ccaData[i].tcpTypeId);
+            Config::Set("/NodeList/*/$ns3::TcpL4Protocol/SocketType", TypeIdValue(tcpTypeId));
+
+            // Create and install applications
+            uint16_t port = 9;
+            BulkSendHelper bulkSend("ns3::TcpSocketFactory", InetSocketAddress(senderReceiverInterfaces.GetAddress(1), port));
+            bulkSend.SetAttribute("MaxBytes", UintegerValue(0)); // Unlimited data
+            ApplicationContainer apps = bulkSend.Install(sender);
+            apps.Start(Seconds(1.0));
+            apps.Stop(Seconds(simulationTime)); // Use the simulationTime parameter
+
+            PacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+            apps = sink.Install(receiver);
+            apps.Start(Seconds(1.0));
+            apps.Stop(Seconds(simulationTime)); // Use the simulationTime parameter
+        }
+    }
 
     Simulator::Run();
     Simulator::Destroy();
---
+++ ns-allinone-3.43/ns-3.43/scratch/adaptive-tcp/adaptive-tcp-test.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <array>
+
+// Enum for CCA constants
+enum CCA {
+    BBRv1,
+    BIC,
+    CUBIC,
+    HTCP,
+    Illinois,
+    NewReno,
+    Vegas,
+    Veno,
+    CCA_COUNT
+};
+
+struct CCAData {
+    int percentage;
+    std::string tcpTypeId;
+};
+
+static const std::array<CCAData, CCA_COUNT> ccaData = {
+    CCAData{17, "ns3::TcpBbr"},      // BBRv1
+    CCAData{4, "ns3::TcpBic"},       // BIC
+    CCAData{52, "ns3::TcpCubic"},    // CUBIC
+    CCAData{4, "ns3::TcpHtcp"},      // HTCP
+    CCAData{5, "ns3::TcpIllinois"},  // Illinois
+    CCAData{12, "ns3::TcpNewReno"},  // NewReno
+    CCAData{6, "ns3::TcpVegas"},     // Vegas
+    CCAData{1, "ns3::TcpVeno"}       // Veno
+};
